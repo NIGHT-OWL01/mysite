462246e8c655842a5a03c845fac62647f83663a3
ganesh <ganeshp7754@gmail.com>
2022-10-02 14:19:11 +0530

    storage models added

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class ContactRelation(models.Model):
+    relation_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.relation_type
+
+class Contact(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    number = models.PositiveIntegerField()
+    relation = models.ForeignKey('ContactRelation',on_delete=models.CASCADE)
